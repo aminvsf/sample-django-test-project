4c86560f78f339a86f5db934e907feb53311e6a7
Amin Vassefpoor <aminvsf911@gmail.com>
2022-05-06 13:33:46 +0430

    Added 'Book' model and 'CreateView' for it in 'app' app

--- app/views.py
+++ app/views.py
@@ -1,5 +1,16 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from app.models import Book
 
 
-class Main(TemplateView):
+class Main(CreateView):
+    model = Book
+    fields = ('name',)
+    success_url = reverse_lazy('main')
     template_name = 'main.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Main, self).get_context_data(**kwargs)
+        context['books'] = Book.objects.all()
+        return context
